e58b895a42d7e52bd304378cf74dd052e39f514d
Eugene Furmanov <codeinsack@gmail.com>
2019-10-27 11:52:09 +0300

    feat: add event listener for main pin

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,18 @@ var $mapPins = document.querySelector('.map__pins');
 var $pinTemplate = document.querySelector('#pin');
 var $cardTemplate = document.querySelector('#card');
 
-$map.classList.remove('map--faded');
-
 var announcements = utils.generateObjects();
 utils.fillMapWithPins(announcements, $pinTemplate, $mapPins);
 var $card = utils.createCard($cardTemplate, announcements[0]);
 
 document.querySelector('.map__filters-container').before($card);
+
+var $pinMain = document.querySelector('.map__pin--main');
+$pinMain.addEventListener('mousedown', function () {
+  $map.classList.remove('map--faded');
+});
+$pinMain.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    $map.classList.remove('map--faded');
+  }
+});
